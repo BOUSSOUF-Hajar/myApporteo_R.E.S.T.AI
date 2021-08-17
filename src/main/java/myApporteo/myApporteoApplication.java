d13810f69e4f578d30914f7fbbc39001dc52cb44
BOUSSOUF-Hajar <hajar-boussouf@hotmail.com>
2021-08-17 10:24:16 +0100

    e

--- src/main/java/myApporteo/myApporteoApplication.java
+++ src/main/java/myApporteo/myApporteoApplication.java
@@ -1,11 +1,16 @@
 package myApporteo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import myApporteo.entities.ERole;
+import myApporteo.entities.Role;
+import myApporteo.repositories.RoleRepository;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,6 +22,16 @@ public class myApporteoApplication {
 	 public BCryptPasswordEncoder bCryptPasswordEncoder() {
 	        return new BCryptPasswordEncoder();
 	    }
-	   
+	 @Bean
+		public CommandLineRunner demo(RoleRepository repository) {
+			return (args) -> {
+				Role roleapp=new Role();
+				roleapp.setName(ERole.ROLE_APPORTEUR);		
+				Role rolepart=new Role();
+				rolepart.setName(ERole.ROLE_PARTENAIRE);		
+				Role roleadmin=new Role();
+				roleadmin.setName(ERole.ROLE_ADMIN);
+			};
+		}
 	
 }
